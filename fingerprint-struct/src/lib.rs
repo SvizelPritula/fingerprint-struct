e1085e2015f5975a6a23c81860e5fb2d93cdb7e8
Benjamin Swart <Benjaminswart@email.cz>
2022-09-20 17:28:43 +0200

    Add fingerprint function

--- fingerprint-struct/src/lib.rs
+++ fingerprint-struct/src/lib.rs
@@ -4,10 +4,15 @@ extern crate alloc;
 
 mod impls;
 
-use digest::Update;
+use digest::{FixedOutput, Output, Update};
 
 pub trait Fingerprint {
     fn fingerprint<U: Update>(&self, hasher: &mut U);
+}
+
+pub fn fingerprint<H: Update + FixedOutput, T: Fingerprint>(value: T, mut hasher: H) -> Output<H> {
+    value.fingerprint(&mut hasher);
+    hasher.finalize_fixed()
 }
 
 #[cfg(feature = "derive")]
